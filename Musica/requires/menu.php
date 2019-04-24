eff040ee78e5b38152705648ae46ab35b5069f9c
Tiago2002 <44277003+Tiago2002@users.noreply.github.com>
2019-04-23 21:47:03 -0300

    Primeiro Commit
    adição de menu e rodapé incompleto

---
+++ Musica/requires/menu.php
@@ -0,0 +1,46 @@
+          <div id="menu">
+              <div id="tit-menu">
+                <h1>Reis do souls</h1>
+              </div>
+              <nav>
+              <div class="nav-redes-sociais">
+                    <ul>
+                        <li>
+                            <a class="github" href="#">
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="twitter" href="#">
+                                <i class="fab fa-twitter-square"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="facebook" href="#">
+                            <i class="fab fa-facebook-square"></i>
+                            </a>
+                        </li>
+                    </ul>
+              </div>
+              
+              <div class="nav-item">
+                  <ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Sobre nós</a></li>
+					<li><a href="#">Musicas</a></li>
+					<li><a href="#">Bandas</a></li>
+                    <li><a href="#">Galeria</a></li>
+                    <li><a href="#">história</a></li>
+                    <li><a href="#">Contato</a></li>
+                  </ul>
+              </div>
+              
+              <div class="nav-cadastro">
+                  <ul>
+                    <li>
+                        <a href="#">Cadastrar-se  <i class="fas fa-sign-in-alt"></i></a>
+                    </li>
+                  </ul>
+              </div>
+          </nav> 
+          </div> 
