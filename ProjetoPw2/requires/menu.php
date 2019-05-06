f95fbe0b99977019218bebd3677ed6bc8a2148eb
Tiago2002 <44277003+Tiago2002@users.noreply.github.com>
2019-05-06 20:43:38 -0300

    mudando coisas

---
+++ ProjetoPw2/requires/menu.php
@@ -0,0 +1,70 @@
+<link rel="stylesheet" href="../css/reset.css">
+<link rel="stylesheet" href="../css/menu.css">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+<nav class="navbar">
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">
+            <i class="fab fa-github"></i>
+            Github
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fab fa-twitter-square"></i>  
+            Twitter
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+             <i class="fab fa-facebook-square"></i>
+            Facebook
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-phone-square"></i>
+            Contato
+        </a>
+      </li>
+    </ul>
+    
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">
+            Home
+            <i class="fas fa-home"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+            Midias
+          <i class="fas fa-play"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+            Galeria
+            <i class="fas fa-list-ul"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+            história
+            <i class="fas fa-puzzle-piece"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+            Cadastrar-se
+            <i class="fas fa-sign-in-alt"></i> 
+        </a>
+      </li>
+    </ul>
+    
+</nav>
---
+++ ProjetoPw2/requires/rodape.php
@@ -0,0 +1,10 @@
+    <div class="rodape">
+        <ul>
+            <li><a href="#"><i class="fab fa-twitter-square"></i> Twitter </a></li>
+            <li><a href="#"><i class="fas fa-at"></i> Contato </a></li>
+            <li><a href="#"><i class="fab fa-github-square"></i> Github </a></li>
+            <li>
+            <p><i class="fas fa-guitar"></i></p>
+            </li>
+        </ul>
+    </div>
