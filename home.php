a033cbb8dbe99b63e569a5aa0a9110de8d9999ea
Tiago2002 <tiagos1401@gmail.com>
2019-05-25 19:41:50 -0300

    começando a home.php

---
+++ home.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/home.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+</head>
+
+<body>
+
+    <section class="sec-main">
+
+        <div class="conteudo">
+
+            <div class="titulo-secao">
+
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                        <h1 class="display-4">últimas Adicionadas</h1>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card-columns">
+                <div class="card">
+                    <div class="foto-conteudo">
+                        <a href="#">
+                            <img class="card-img-top" src="imagens/01.jpg" alt="Imagem de capa do card">
+                            <h6 class="tit-conteudo">Battlefield 1 Apocalypse</h6>
+                            <span class="nome-criadora">EA Games</span>
+                            <span class="data-conteudo">01/02/2002</span>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="foto-conteudo">
+                        <a href="#">
+                            <img class="card-img-top" src="imagens/01.jpg" alt="Imagem de capa do card">
+                            <h6 class="tit-conteudo">Battlefield 1</h6>
+                            <span class="nome-criadora">EA Games</span>
+                            <span class="data-conteudo">01/02/2002</span>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="foto-conteudo">
+                        <a href="#">
+                            <img class="card-img-top" src="imagens/01.jpg" alt="Imagem de capa do card">
+                            <h6 class="tit-conteudo">Battlefield 1</h6>
+                            <span class="nome-criadora">EA Games</span>
+                            <span class="data-conteudo">01/02/2002</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
